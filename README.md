ddb7e7487f3235800eba0df9302e076cb3afa6d8
MatthewScott-jpg <scottydotty333@gmail.com>
2021-09-16 22:55:51 -0400

    Implemeted waitgroups, routines, modified default urls

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ This project was created by Matthew Scott, Liam Potts, and Roland Afzelius.
 Inspiration taken from this link for running command line prompts: https://play.golang.org/p/oCVwgamyNg
 
 Waitgroup learning is here https://medium.com/@greenraccoon23/multi-thread-for-loops-easily-and-safely-in-go-a2e915302f8b
+
+Some of these were used
+https://stackoverflow.com/questions/36056615/what-is-the-advantage-of-sync-waitgroup-over-channels
+https://play.golang.org/p/oCVwgamyNg
+https://play.golang.org/p/A2yALLGJgZ
--- main.go
+++ main.go
@@ -4,33 +4,159 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"runtime"
+	"sync"
 	"time"
 )
 
-func PingHost(url string, ch chan string) {
-	if url == "" {
-		ch <- ""
+func PingHost(out chan string, urls []string) {
+	//runtime.GOMAXPROCS(6)
+	urlsLength := len(urls)
+
+	var wg sync.WaitGroup
+	wg.Add(urlsLength)
+
+	fmt.Println("Running for loop...")
+	for i := 0; i < urlsLength; i++ {
+		go func(i int) {
+			defer wg.Done()
+			n := urls[i]
+			cmd, ok := exec.Command("ping", "-c3", n).Output()
+			if ok != nil {
+				out <- n + " not found"
+			} else {
+				out <- string(cmd)
+			}
+		}(i)
 	}
-	//add error handling
-	cmd, _ := exec.Command("ping", "-c3", url).Output()
-	ch <- string(cmd)
+	wg.Wait()
 }
 
 func main() {
-	//runtime.GOMAXPROCS(4)
-	ch := make(chan string)
-	urls := [5]string{
-		"google.com", "cisco.com", "ya.ru", "mail.ru", "golang.org",
+	runtime.GOMAXPROCS(4)
+	urls := []string{
+		"google.com",
+		"google.co.jp",
+		"google.co.uk",
+		"google.es",
+		"google.ca",
+		"google.de",
+		"google.it",
+		"google.fr",
+		"google.com.au",
+		"google.com.tw",
+		"google.nl",
+		"google.com.br",
+		"google.com.tr",
+		"google.be",
+		"google.com.gr",
+		"google.co.in",
+		"google.com.mx",
+		"google.dk",
+		"google.com.ar",
+		"google.ch",
+		"google.cl",
+		"google.at",
+		"google.co.kr",
+		"google.ie",
+		"google.com.co",
+		"google.pl",
+		"google.pt",
+		"google.com.pk",
 	}
+
+	out := make(chan string, len(urls))
 	start := time.Now()
 
-	//for loop sequentially runs the routines
-	//figure out the mistake here: how to prevent waiting in the for loop
-	for i := range urls {
-		go PingHost(urls[i], ch)
-		fmt.Println(<-ch)
+	PingHost(out, urls)
+
+	for range urls {
+		fmt.Println(<-out)
 	}
+
 	duration := time.Since(start)
 	fmt.Println("time", duration)
 
 }
+
+/*
+package main
+
+//https://medium.com/@greenraccoon23/multi-thread-for-loops-easily-and-safely-in-go-a2e915302f8b
+import (
+	"fmt"
+	"os/exec"
+	"sync"
+	"time"
+)
+
+func PingHost(out chan string, urls []string){
+	//runtime.GOMAXPROCS(6)
+	urlsLength := len(urls)
+
+	var wg sync.WaitGroup
+	wg.Add(urlsLength)
+
+	fmt.Println("Running for loop...")
+	for i := 0; i < urlsLength; i++ {
+		go func(i int) {
+			defer wg.Done()
+			n := urls[i]
+			cmd, ok := exec.Command("ping", "-c3", n).Output()
+			if ok != nil {
+				out <- n + " not found"
+			}else {
+				out <- string(cmd)
+			}
+		}(i)
+	}
+	wg.Wait()
+}
+
+func main() {
+	urls := []string{
+		"https://www.Google.com",
+		"https://www.google.co.jp",
+		"https://www.google.co.uk",
+		"https://www.google.es",
+		"https://www.google.ca",
+		"https://www.google.de",
+		"https://www.google.it",
+		"https://www.google.fr",
+		"https://www.google.com.au",
+		"https://www.google.com.tw",
+		"https://www.google.nl",
+		"https://www.google.com.br",
+		"https://www.google.com.tr",
+		"https://www.google.be",
+		"https://www.google.com.gr",
+		"https://www.google.co.in",
+		"https://www.google.com.mx",
+		"https://www.google.dk",
+		"https://www.google.com.ar",
+		"https://www.google.ch",
+		"https://www.google.cl",
+		"https://www.google.at",
+		"https://www.google.co.kr",
+		"https://www.google.ie",
+		"https://www.google.com.co",
+		"https://www.google.pl",
+		"https://www.google.pt",
+		"google.com.pk",
+	}
+
+	out := make(chan string, len(urls))
+	start := time.Now()
+
+	PingHost(out, urls)
+
+	for range urls{
+		fmt.Println(<-out)
+	}
+
+	duration := time.Since(start)
+	fmt.Println("time", duration)
+
+}
+
+*/
