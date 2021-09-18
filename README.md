21ae004015c2303f3f3b395b0043569004c4d59c
MatthewScott-jpg <scottydotty333@gmail.com>
2021-09-18 12:12:37 -0400

    Removed waitgroups, now solely use goroutines and channels

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Inspiration taken from this link for running command line prompts: https://play.
 Waitgroup learning is here https://medium.com/@greenraccoon23/multi-thread-for-loops-easily-and-safely-in-go-a2e915302f8b
 
 Some of these were used
+
+This one has the notes about wg and channels
 https://stackoverflow.com/questions/36056615/what-is-the-advantage-of-sync-waitgroup-over-channels
+
+
 https://play.golang.org/p/oCVwgamyNg
 https://play.golang.org/p/A2yALLGJgZ
--- main.go
+++ main.go
@@ -1,25 +1,20 @@
 package main
 
-//https://medium.com/@greenraccoon23/multi-thread-for-loops-easily-and-safely-in-go-a2e915302f8b
+//TODO: Add user input, store performance statistics in map, iterate through gomaxprocs possibilities
+
 import (
 	"fmt"
 	"os/exec"
 	"runtime"
-	"sync"
 	"time"
 )
 
 func PingHost(out chan string, urls []string) {
-	//runtime.GOMAXPROCS(6)
 	urlsLength := len(urls)
-
-	var wg sync.WaitGroup
-	wg.Add(urlsLength)
 
 	fmt.Println("Running for loop...")
 	for i := 0; i < urlsLength; i++ {
 		go func(i int) {
-			defer wg.Done()
 			n := urls[i]
 			cmd, ok := exec.Command("ping", "-c3", n).Output()
 			if ok != nil {
@@ -29,11 +24,10 @@ func PingHost(out chan string, urls []string) {
 			}
 		}(i)
 	}
-	wg.Wait()
 }
 
 func main() {
-	runtime.GOMAXPROCS(4)
+	runtime.GOMAXPROCS(8)
 	urls := []string{
 		"google.com",
 		"google.co.jp",
@@ -68,7 +62,7 @@ func main() {
 	out := make(chan string, len(urls))
 	start := time.Now()
 
-	PingHost(out, urls)
+	go PingHost(out, urls)
 
 	for range urls {
 		fmt.Println(<-out)
@@ -76,87 +70,4 @@ func main() {
 
 	duration := time.Since(start)
 	fmt.Println("time", duration)
-
 }
-
-/*
-package main
-
-//https://medium.com/@greenraccoon23/multi-thread-for-loops-easily-and-safely-in-go-a2e915302f8b
-import (
-	"fmt"
-	"os/exec"
-	"sync"
-	"time"
-)
-
-func PingHost(out chan string, urls []string){
-	//runtime.GOMAXPROCS(6)
-	urlsLength := len(urls)
-
-	var wg sync.WaitGroup
-	wg.Add(urlsLength)
-
-	fmt.Println("Running for loop...")
-	for i := 0; i < urlsLength; i++ {
-		go func(i int) {
-			defer wg.Done()
-			n := urls[i]
-			cmd, ok := exec.Command("ping", "-c3", n).Output()
-			if ok != nil {
-				out <- n + " not found"
-			}else {
-				out <- string(cmd)
-			}
-		}(i)
-	}
-	wg.Wait()
-}
-
-func main() {
-	urls := []string{
-		"https://www.Google.com",
-		"https://www.google.co.jp",
-		"https://www.google.co.uk",
-		"https://www.google.es",
-		"https://www.google.ca",
-		"https://www.google.de",
-		"https://www.google.it",
-		"https://www.google.fr",
-		"https://www.google.com.au",
-		"https://www.google.com.tw",
-		"https://www.google.nl",
-		"https://www.google.com.br",
-		"https://www.google.com.tr",
-		"https://www.google.be",
-		"https://www.google.com.gr",
-		"https://www.google.co.in",
-		"https://www.google.com.mx",
-		"https://www.google.dk",
-		"https://www.google.com.ar",
-		"https://www.google.ch",
-		"https://www.google.cl",
-		"https://www.google.at",
-		"https://www.google.co.kr",
-		"https://www.google.ie",
-		"https://www.google.com.co",
-		"https://www.google.pl",
-		"https://www.google.pt",
-		"google.com.pk",
-	}
-
-	out := make(chan string, len(urls))
-	start := time.Now()
-
-	PingHost(out, urls)
-
-	for range urls{
-		fmt.Println(<-out)
-	}
-
-	duration := time.Since(start)
-	fmt.Println("time", duration)
-
-}
-
-*/
